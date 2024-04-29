96a6a2247673c2133aefd0cdc03f8f27bf6e0bc8
baqnngg <bbqdnrmas@gmail.com>
2024-04-29 20:44:43 +0900

    123

--- 4_29/main.js
+++ 4_29/main.js
@@ -1,7 +1,40 @@
-var a = prompt();
-if (a <= 12){
-    console.log("오전");
+// 1번째
+// var a = prompt();
+// if (a < 12){
+//     console.log("오전");
+// }
+// else {
+//     console.log("오후");
+// }
+
+// 2번째
+// var money = prompt();
+// document.write(money*1300,"원");
+
+// 3번째
+
+let n = Number(prompt());
+
+if (1<=n && n < 2){
+    document.write("플랑크톤");
 }
-else {
-    console.log("오후");
+else if (2<=n && n<3){
+    document.write("돌고래");
 }
+else if (3<=n && n<4){
+    document.write("인간");
+}
+else if (3<=n && n<4.5){
+    document.write("교수님");
+}
+else if (4.5 == n){
+    document.write("신");
+}
+
+// let A = "aaa";
+// console.log(typeof(A));
+// let a = "가나다"+"라마바사";
+// console.log(a);
+// let num = 123+456;
+// console.log(num);
+
