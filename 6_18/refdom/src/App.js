2311ba5730d3856c96964ffb4f832b2a08a300d9
baqnngg <il8696546@gmail.com>
2024-06-18 21:13:49 +0900

    RefDom

--- 6_18/refdom/src/App.js
+++ 6_18/refdom/src/App.js
@@ -1,22 +1,16 @@
-// import ValidationSample from './validationSample';
+import ValidationSample from './validationSample';
 import ScrollBox from './scrollBox';
 import { Component } from 'react';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       {/* <h1>비밀번호를 맞추세요 (4자리 숫자)</h1> */}
-//       {/* <h3>힌트 - F12</h3> */}
-//       {/* 비밀번호는 000 */}
-//       {/* <ValidationSample/> */}
-//     </div>
-//   );
-// }
 
 class App extends Component{
   render() {
     return (
       <div>
+          <h1>비밀번호를 맞추세요 (4자리 숫자)</h1>
+          <h3>힌트 - F12</h3>
+          비밀번호는 000
+          <ValidationSample/>
+          <p>=============================================================================================================================================================================================================================</p>
           <ScrollBox ref={(ref) => this.scrollBox=ref}/>
           <button onClick={() => this.scrollBox.scrollToBottom()}>
             맨 밑으로
