dac22f40f084dd573599ac37f554037c71422bab
baqnngg <il8696546@gmail.com>
2024-06-18 20:10:40 +0900

    jung sik

---
+++ 6_18/refdom/src/App.js
@@ -0,0 +1,12 @@
+import ValidationSample from './validationSample';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>비밀번호를 맞추세요 (4자리 숫자)</h1>
+      <ValidationSample/>
+    </div>
+  );
+}
+
+export default App;
