7979fc345014eea2fa9ac0e6d65c26182d977fbb
baqnngg <il8696546@gmail.com>
2024-06-25 20:11:34 +0900

    myblog

--- 6_25/myblog/src/App.js
+++ 6_25/myblog/src/App.js
@@ -3,7 +3,7 @@ import {useState} from 'react';
 
 function App() {
   // let [a,b] : array에 있는 데이터를 변수로 저장하고 싶을때 사용
-  let [글제목,b] = useState(['남자 코트 추천', '강남 우동 맛집', '알고리즘 독학']);
+  let [글제목,글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '알고리즘 독학']);
   let [따봉,따봉변경] = useState(0);
 
   return (
@@ -12,6 +12,13 @@ function App() {
         
         <div>개발 blog</div>
       </div>
+
+      <button onClick={() => {
+        let cp = [...글제목];
+        cp[0] = '여자 코트 추천';
+        글제목변경(cp);
+      }}>글 수정</button>
+
       <div className='list'>
         <h4>{글제목[0]}<span onClick={() => { 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
         <p>6월 25일 작성</p>
