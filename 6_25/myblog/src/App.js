2c5a87243d4644c410a58c12ca775a6a6aa8ff8e
baqnngg <il8696546@gmail.com>
2024-06-25 21:06:50 +0900

    myblog

--- 6_25/myblog/src/App.js
+++ 6_25/myblog/src/App.js
@@ -14,10 +14,17 @@ function App() {
       </div>
 
       <button onClick={() => {
+        // ...을 붙히면 화살표가 달라짐
         let cp = [...글제목];
         cp[0] = '여자 코트 추천';
         글제목변경(cp);
       }}>글 수정</button>
+
+      <button onClick={() => {
+        let cp = [...글제목];
+        cp.sort();  
+        글제목변경(cp)
+      }}>정렬 버튼</button>
 
       <div className='list'>
         <h4>{글제목[0]}<span onClick={() => { 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
@@ -31,8 +38,21 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>6월 25일 작성</p>
       </div>
+      <Modal/>
     </div>
   );
 }
-
+// 컴포넌트로 만들면 좋은 것들 
+// 1. 반복적인 HTML 축약할때
+// 2. 큰 페이지
+// 3. 자주 변경되는 것들
+function Modal() {
+  return (
+    <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+  )
+}
 export default App;
